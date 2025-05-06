9a47eecfe3f7737aa799f9c8d221ee8e295e489f
charan6018 <148361768+charan6018@users.noreply.github.com>
2025-05-06 14:26:00 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,21 +8,6 @@ const Footer = () => {
         <p className="text-sm text-gray-300 mb-4">
          Neuronest is an AI-powered mental wellness assistant designed to provide supportive conversations, emotional check-ins, and self-care guidance anytime you need it.
         </p>
-        <div className="flex justify-center space-x-4 mb-3">
-          <a
-            href="https://github.com/codeaashu/Helpmate-AI"
-            className="text-blue-400 hover:text-white transform hover:scale-110 transition duration-300 flex items-center"
-          >
-            <FaGithub className="mr-1" /> GitHub
-          </a>
-          <span className="text-gray-500">|</span>
-          <a
-            href="https://ai.google.dev/competition/projects/helpmate-ai"
-            className="text-blue-400 hover:text-white transform hover:scale-110 transition duration-300 flex items-center"
-          >
-            <FaRocket className="mr-1" /> Vote Now
-          </a>
-        </div>
         <p className="text-xs text-gray-500">PROJECT DONE BY KISHORE H, BHUVANESH B and CHARAN C.</p>
       </div>
     </footer>
